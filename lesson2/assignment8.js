1f3a81609c76f1769860db7da5da6e06f08033f0
Christopher Perkins <Christopher.Perkins@null.net>
2023-06-08 18:01:57 -0400

    L2A8: Further Explored

--- lesson2/assignment8.js
+++ lesson2/assignment8.js
@@ -13,6 +13,8 @@ console.log(isBalanced('Hey!'));                   // true
 console.log(isBalanced(')Hey!('));                 // false
 console.log(isBalanced('What ((is))) up('));       // false
 
+/*
+// original
 function isBalanced(string) {
   const parentheses = string.replace(/[^()]/g, '');
   let opened = 0;
@@ -23,4 +25,28 @@ function isBalanced(string) {
   }
 
   return opened === 0;
+}
+*/
+
+// Further Exploration:
+// Can you refactor the current solution to work with the forEach method? Why
+// does our current solution work with a for loop, but not with forEach?
+
+// Our current solution worked with for, because it was a string and it needs
+// to exit the loop prior to being done in a case. We can refactor it to do so
+// by splitting the string into an array of characters and to keep track if it
+// failed during the iterations. Unfortunately, we'd always check every
+// character and that is kind of a waste.
+
+function isBalanced(string) {
+  const parentheses = string.replace(/[^()]/g, '').split('');
+  let opened = 0;
+  let good = true;
+
+  parentheses.forEach((char) => {
+    opened = char === '(' ? opened + 1 : opened - 1;
+    if (opened < 0) good = false;
+  });
+
+  return good && opened === 0;
 }
