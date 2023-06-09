83046dc82696beaa555a035535a7845dd0b7e54d
Christopher Perkins <Christopher.Perkins@null.net>
2023-06-09 09:36:16 -0400

    Exercises: String Processing 2 solved

---
+++ exercises/stringProcessing/exercise2.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array of strings and returns an array of the
+// same string values, but with all vowels (a, e, i, o, u) removed.
+
+// Examples:
+
+removeVowels(['abcdefghijklmnopqrstuvwxyz']);         // ["bcdfghjklmnpqrstvwxyz"]
+removeVowels(['green', 'YELLOW', 'black', 'white']);  // ["grn", "YLLW", "blck", "wht"]
+removeVowels(['ABC', 'AEIOU', 'XYZ']);                // ["BC", "", "XYZ"]
+
+function removeVowels(array) {
+  return array.map(string => string.replace(/[aeiou]/gi, ''));
+}
